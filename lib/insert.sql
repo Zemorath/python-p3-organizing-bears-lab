6710d44c80561aa9a784711e791bdc6a72d4b5c4
Zemorath <trentward10@gmail.com>
2023-12-11 14:50:09 -0600

    Bears Lab Complete

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 39, 'M', 'Brown', 'Lazy', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 27, 'MF', 'Polar', 'Rambunctious', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabith', 3, 'F', 'Polar', 'Curious', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 46, 'M', 'Brown', 'Hostile', FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 15, 'F', 'Black', 'Hostile', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 30, 'M', 'Polar', 'Passive', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'F', 'Black', 'Lazy', FALSE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 40, 'M', 'Panda', 'Lazy', TRUE);
